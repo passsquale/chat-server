395c1748438b7262bdcebb19bf2de20167e308b4
Росляков Евгений <roslyakov.e2@wb.ru>
2024-06-07 10:39:44 +0300

    Add sample grpc server

--- cmd/grpc_server/main.go
+++ cmd/grpc_server/main.go
@@ -1,7 +1,49 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"github.com/brianvoe/gofakeit"
+	"github.com/golang/protobuf/ptypes/empty"
+	desc "github.com/passsquale/chat-server/pkg/chat_v1"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+	"log"
+	"net"
+)
+
+const grpcPort = 50052
+
+type server struct {
+	desc.UnimplementedChatV1Server
+}
+
+func (s *server) Create(ctx context.Context, req *desc.CreateRequest) (*desc.CreateResponse, error) {
+	log.Printf("Create chat: %v", req.GetUsernames())
+	return &desc.CreateResponse{Id: gofakeit.Int64()}, nil
+}
+
+func (s *server) Delete(ctx context.Context, req *desc.DeleteRequest) (*empty.Empty, error) {
+	log.Printf("Delete chat: %d", req.GetId())
+	return &empty.Empty{}, nil
+}
+
+func (s *server) SendMessage(ctx context.Context, req *desc.SendMessageRequest) (*empty.Empty, error) {
+	log.Printf("Sending message: %+v", req.GetMessage())
+	return &empty.Empty{}, nil
+}
 
 func main() {
-	fmt.Println("Hello, world")
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", grpcPort))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	reflection.Register(s)
+	desc.RegisterChatV1Server(s, &server{})
+
+	log.Printf("server listening at %s", lis.Addr())
+	if err = s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
 }
